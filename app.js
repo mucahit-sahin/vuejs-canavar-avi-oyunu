cf91e31d54f977858b7823a8e73630b724870158
mucahit-sahin <mucahitsahin786@gmail.com>
2020-11-07 18:22:03 +0300

    property ve metodlar oluşturuldu

---
+++ app.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:"#app",
+    data:{
+        player_heal:100,
+        monster_heal:100,
+    },
+    methods:{
+        start_game:function(){
+        },
+        attack:function(){
+
+        },
+        special_attack:function(){
+
+        },
+        heal_up:function(){
+
+        },
+        give_up:function(){
+            
+        }
+    }
+})
