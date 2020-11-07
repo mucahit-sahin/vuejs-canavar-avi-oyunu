8bbc84b73caa053ba90ff9d6e4f2bbcf0595f588
mucahit-sahin <mucahitsahin786@gmail.com>
2020-11-07 22:28:53 +0300

    Can sınırları ayarlandı ve oyun sonucu yapıldı

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ new Vue({
             this.monster_heal-=monsterDamage
         },
         special_attack:function(){
-            let playerDamage= Math.ceil(Math.random()*25)
+            let playerDamage= Math.ceil(Math.random()*10)
             this.player_heal-=playerDamage
-            let monsterDamage= Math.ceil(Math.random()*15)
+            let monsterDamage= Math.ceil(Math.random()*25)
             this.monster_heal-=monsterDamage
         },
         heal_up:function(){
@@ -28,5 +28,32 @@ new Vue({
         give_up:function(){
             
         }
+    },
+    watch:{
+        player_heal:function(){
+            if(this.player_heal>100){
+                this.player_heal=100
+            }else if(this.player_heal<0){
+                this.player_heal=0
+                if(confirm("Canavar Kazandı! Tekrar oynayalım mı?"))
+                {
+                    this.player_heal=100
+                    this.monster_heal=100
+                }
+            }
+        },
+        monster_heal:function(){
+            if(this.monster_heal>100){
+                this.monster_heal=100
+            }else if(this.monster_heal<0){
+                this.monster_heal=0
+                if(confirm("Player Kazandı! Tekrar oynayalım mı?"))
+                {
+                    this.player_heal=100
+                    this.monster_heal=100
+                }
+            }
+        }
     }
+
 })
